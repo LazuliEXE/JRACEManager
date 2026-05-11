139fdad45503662e5c17b2ec4254f177a1c7e01e
LazuliEXE <lazulitechexe@gmail.com>
2026-05-11 16:29:02 +0200

    Initialisation projet JavaFX + Maven

---
+++ src/main/java/fr/lhsa/Main.java
@@ -0,0 +1,24 @@
+package fr.lhsa;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+
+        Label label = new Label("JRESManager");
+
+        Scene scene = new Scene(label, 800, 600);
+
+        stage.setTitle("JRESManager");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
